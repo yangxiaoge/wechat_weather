b82618c59f5beff8e6e7899bad70b404aad9a0f7
yangjianan <yangjianan@seuic.com>
2018-09-19 16:37:37 +0800

    关于页面添加 “ReadHub热门话题”

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,60 +1,27 @@
 // pages/about/about.js
 //获取应用实例
 const app = getApp()
-
+var visiable = false
 Page({
-  data: {
-    motto: 'Hi\n这是 ZzWeather 关于页面\n更多内容敬请期待！^_^\n\nhttps://github.com/yangxiaoge/wechat_weather',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+  data:{
+    aboutApp: 'Hi\n这是 ZzWeather 关于页面 \n 更多内容敬请期待！^_^\n\nhttps://github.com/yangxiaoge/wechat_weather\n\n下面是ReadHub热门话题',
   },
 
   //跳转log 页面
-  viewLog: function () {
+  viewLog: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  onLoad: function() {
+    // 获取 readhub 热门话题
+    this.getHotTopics()
   },
 
   // 转发分享小程序
-  onShareAppMessage: function (res) {
+  onShareAppMessage: function(res) {
     if (res.from === 'button') {
       // 来自页面内转发按钮
       console.log(res.target)
@@ -62,14 +29,14 @@ Page({
     return {
       title: 'Zz 天气预报',
       path: '/pages/index/index',
-      success: function (res) {
+      success: function(res) {
         wx.showToast({
           title: '转发成功',
           icon: 'success',
           duration: 2000
         })
       },
-      fail: function (res) {
+      fail: function(res) {
         // 转发失败取消提示
         // wx.showToast({
         //   title: '转发失败',
@@ -78,6 +45,24 @@ Page({
         // })
       }
     }
+  },
+
+  getHotTopics: function() {
+    var that = this;
+    var url = "https://api.readhub.cn/topic";
+
+    wx.request({
+      url: url,
+      success: function(res) {
+        console.log(JSON.stringify(res));
+
+        that.setData({
+          hottopic_datas: res.data.data
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   },
 
 })
